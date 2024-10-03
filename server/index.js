ee38f6105491cbdb2244bd52ac66ad6fef84500d
DIAZ <owenstokd@gmail.com>
2024-10-02 20:11:47 -0400

    prisma

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ import cors from "cors";
 import {dirname, join} from 'path'
 import {fileURLToPath} from 'url'
 import { PORT } from "./config.js";
-
 import indexRoutes from "./routes/index.routes.js";
 import taskRoutes from "./routes/tasks.routes.js";
+import { PrismaClient } from "@prisma/client/extension";
 
+const prismaClient = require('@prisma/client');
+const prisma = new PrismaClient();
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 console.log(__dirname)
