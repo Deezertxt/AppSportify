a463b6c2a0b78b2b2e4c1f984e82696756122f12
RodrigoVLRC <RodrigoVR70@gmail.com>
2024-10-03 22:30:27 -0400

    add: navbar footer reproductor to react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import {Route, Routes} from  'react-router-dom';
 import TaskPage from "./pages/Taskpage";
+import Reproductor from "./pages/Reproductor.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
     return(
-        <Routes>
-            <Route path="/" element={<TaskPage/>}/>
-        </Routes>
+        <>
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<TaskPage/>}/>
+                <Route path="/reproductor" element={<Reproductor/>}/>
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+            <Footer/>
+        </>
     )
     }
     export default App
