2843f181e885515aeee5983c8f1676d6f2c40f9d
RodrigoVLRC <RodrigoVR70@gmail.com>
2024-10-04 21:28:16 -0400

    fix: button bg color

--- client/src/pages/Biblioteca.jsx
+++ client/src/pages/Biblioteca.jsx
@@ -20,7 +20,7 @@ function Biblioteca() {
                                className="block w-full p-4 ps-10 text-sm text-gray-900 border rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
                                placeholder="Buscar futbol, basquet..." required/>
                         <button type="submit"
-                                className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buscar
+                                className="text-white absolute end-2.5 bottom-2.5 bg-[#6177A6] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Buscar
                         </button>
                     </div>
                 </form>
