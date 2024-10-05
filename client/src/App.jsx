8d48aceea79ba6080ecd327cdc5f332caa67fa0a
RodrigoVLRC <RodrigoVR70@gmail.com>
2024-10-05 12:37:45 -0400

    add: publicar view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import {Route, Routes} from  'react-router-dom';
 import TaskPage from "./pages/Taskpage";
 import Reproductor from "./pages/Reproductor.jsx";
 import Biblioteca from "./pages/Biblioteca.jsx";
+import Publicar from "./pages/Publicar.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
@@ -14,6 +15,7 @@ function App() {
                 <Route path="/" element={<TaskPage/>}/>
                 <Route path="/reproductor" element={<Reproductor/>}/>
                 <Route path="/biblioteca" element={<Biblioteca/>}/>
+                <Route path="/publicar" element={<Publicar/>}/>
                 <Route path="*" element={<NotFound/>}/>
             </Routes>
             <Footer/>
