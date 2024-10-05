6fe4a95f4d400d6f4ae8957b9ddc04ef208beb67
RodrigoVLRC <RodrigoVR70@gmail.com>
2024-10-05 12:54:18 -0400

    fix-add: navbar, added Equipos view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import TaskPage from "./pages/Taskpage";
 import Reproductor from "./pages/Reproductor.jsx";
 import Biblioteca from "./pages/Biblioteca.jsx";
 import Publicar from "./pages/Publicar.jsx";
+import Equipos from "./pages/Equipos.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
@@ -16,6 +17,7 @@ function App() {
                 <Route path="/reproductor" element={<Reproductor/>}/>
                 <Route path="/biblioteca" element={<Biblioteca/>}/>
                 <Route path="/publicar" element={<Publicar/>}/>
+                <Route path="/equipos" element={<Equipos/>}/>
                 <Route path="*" element={<NotFound/>}/>
             </Routes>
             <Footer/>
