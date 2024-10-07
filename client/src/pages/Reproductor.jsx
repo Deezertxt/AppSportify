aaeeeebf0e4d45e549267dc3de83b37a334e6355
RodrigoVLRC <RodrigoVR70@gmail.com>
2024-10-06 21:02:18 -0400

    fix: svg play button

--- client/src/pages/Reproductor.jsx
+++ client/src/pages/Reproductor.jsx
@@ -139,10 +139,11 @@ function Reproductor() {
                                     <line x1="14" x2="14" y1="15" y2="9"></line>
                                 </svg>
                             ) : (
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
-                                     stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                                     className="lucide lucide-play h-16 w-16">
-                                    <polygon points="5 3 19 12 5 21 5 3"></polygon>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                                     fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                                     strokeLinejoin="round" className="lucide lucide-circle-play h-16 w-16">
+                                    <circle cx="12" cy="12" r="10"></circle>
+                                    <polygon points="10 8 16 12 10 16 10 8"></polygon>
                                 </svg>
                             )}
                         </button>
