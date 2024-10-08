c8f1853dce806febdaf21d9b30607a39341b9169
DIAZ <owenstokd@gmail.com>
2024-10-08 01:29:51 -0400

    fixed: filtrar caracteres chafas

--- server/utils/extraerTexto.js
+++ server/utils/extraerTexto.js
@@ -2,12 +2,20 @@
 import fs from 'fs';
 import pdf from 'pdf-parse/lib/pdf-parse.js';
 
+const limpiarTexto = (texto) => {
+  // Expresión regular para eliminar caracteres no alfabéticos, excepto espacios
+  return texto.replace(/[*^#@!$%&()_+=\[\]{};':"\\|,.<>?\/`~]+/g, ' ') // Elimina asteriscos y otros caracteres especiales
+              .replace(/\s+/g, ' ') // Reemplaza múltiples espacios por uno solo
+              .trim(); // Elimina espacios al principio y al final
+};
+
 const extraerTexto = async (ruta) => {
     try {
         const dataBuffer = fs.readFileSync(ruta); 
-        const data = await pdf(dataBuffer);            
-        console.log(data.text);                        
-        return data.text;
+        const data = await pdf(dataBuffer);
+        const textoLimpio = limpiarTexto(data.text);            
+        console.log(textoLimpio);                        
+        return textoLimpio;
       } catch (error) {
         console.error('Error al extraer texto del PDF:', error);
       }
