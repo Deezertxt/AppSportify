53232ab9d3eec74771b1adb8f65d727b9626dd73
zebal <zeballosperedodylanamin@gmail.com>
2024-10-09 03:52:44 -0400

    feat:image pdf modal

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -25,7 +25,7 @@ export const createCategory = (data) => api.post("/api/category/create", data);
 export const updateCategory = (id, data) => api.put(`/api/category/update/${id}`, data);
 export const deleteCategory = (id) => api.delete(`/api/category/delete/${id}`);
 export const getUploads = () => api.get("/api/upload/get");
-export const uploadFile = (data) => api.post("/api/upload", data);
+export const uploadFiles = (data) => api.post("/api/uploads/upload", data);
 export const getAudiobooks = () => api.get("/api/audiobook/get");
 export const getAudiobook = (id) => api.get(`/api/audiobook/get/${id}`);
 export const createAudiobook = (data) => api.post("/api/audiobook/register", data);
