d2ccdf820cc95fe57602f436a13a1dbe2f7db160
zebal <zeballosperedodylanamin@gmail.com>
2024-10-08 21:51:37 -0400

    feat:conecction front-back

--- server/src/routes/audiobook.routes.js
+++ server/src/routes/audiobook.routes.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const {
     createAudiobook,
     deleteAudioBook,
+    getAudiobooks,
     getAudioBookById,
     updateAudiobook,
 } = require("../controller/audiobookController");
 
 router.post("/register", createAudiobook);
 router.delete("/delete/:id", deleteAudioBook);
+router.get("/get", getAudiobooks);
 router.get("/get/:id", getAudioBookById);
 router.put("/put/:id", updateAudiobook);
 
