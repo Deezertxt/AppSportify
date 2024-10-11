02b9a68ad5cba0808cff18b5b718265fb61722b2
dylan <dylan@MacBook-Air-de-dylan.local>
2024-10-10 20:02:44 -0400

    preuba

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,21 +4,21 @@ import { FaUserCircle } from 'react-icons/fa';
 function Navbar() {
    
     return (
-        <nav className="bg-[#6177A6] text-white text-lg flex items-center justify-between py-4 px-10">
+        <nav className="bg-[#0B6477] text-white text-lg flex flex-col lg:flex-row items-center justify-between py-4 px-10">
             <div>
                 <img className="block lg:hidden h-20 w-18" src="./logoS.svg" alt="Logo" />
                 <img className="hidden lg:block h-20 w-auto" src="./logoS.svg" alt="Logo" />
             </div>
 
-            <div>
-                <ul className="flex gap-24">
+            <div className="flex flex-col lg:flex-row lg:items-center">
+                <ul className="flex flex-col lg:flex-row gap-4 lg:gap-24">
                     <li>
                         <Link to={"/"} className="hover:text-gray-400 transition duration-250 ease-in-out">Inicio</Link>
                     </li>
 
                     <li>
                         <Link to={"publicar"}
-                            className="hover:text-gray-400 transition duration-250 ease-in-out">Publicar</Link>
+                            className="hover:text-gray-400 transition duration-250 ease-in-out">Registrar</Link>
                     </li>
 
                     <li>
@@ -32,14 +32,14 @@ function Navbar() {
                     </li>
 
                 </ul>
-            </div>
 
-            <div className="flex items-center gap-3 font-bold">
-                <FaUserCircle size={40} />
-                <div>
-                    <p className="ml-4 py-2 leading-5 font-medium font-semibold hover:text-gray-300 transition duration-250 ease-in-out">
-                        Nombre de Usuario
-                    </p>
+                <div className="flex items-center gap-3 font-bold">
+                    <FaUserCircle size={40} />
+                    <div>
+                        <p className="ml-4 py-2 leading-5 font-medium font-semibold hover:text-gray-300 transition duration-250 ease-in-out">
+                            Nombre de Usuario
+                        </p>
+                    </div>
                 </div>
             </div>
         </nav>
