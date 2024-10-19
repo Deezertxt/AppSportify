a81144feeda31186ac7ac8485251377898914774
Shoutt4 <jhonvergara437@gmail.com>
2024-10-18 20:11:56 -0400

    mayusculas en titulos

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+function concertir(cadena) {
+    return cadena
+        .split(" ")
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ");
+}
+
 function Card({ title, author, description, coverUrl, onClick }) {
     return (
         <div 
@@ -10,7 +17,7 @@ function Card({ title, author, description, coverUrl, onClick }) {
             <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:shadow-lg">
                 <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={coverUrl} alt="audiobook cover" />
                 <div className="p-6">
-                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3">{title}</h1>
+                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3">{concertir(title)}</h1>
                     <h2 className="tracking-widest text-s title-font font-medium text-gray-400 mb-1">{author}</h2>
                     <p className="leading-relaxed mb-3">{description}</p>
                 </div>
