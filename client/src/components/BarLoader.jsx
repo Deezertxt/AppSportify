ebe12c0d57195628acaca55968b237da09eac26a
zebal <zeballosperedodylanamin@gmail.com>
2024-10-18 20:09:01 -0400

    fix:bugs errors

---
+++ client/src/components/BarLoader.jsx
@@ -0,0 +1,52 @@
+import { motion } from "framer-motion";
+
+const variants = {
+  initial: {
+    scaleY: 0.5,
+    opacity: 0,
+  },
+  animate: {
+    scaleY: 1,
+    opacity: 1,
+    transition: {
+      repeat: Infinity,
+      repeatType: "mirror",
+      duration: 1,
+      ease: "circIn",
+    },
+  },
+};
+
+const BarLoader = () => {
+  return (
+    <motion.div
+      transition={{
+        staggerChildren: 0.25,
+      }}
+      initial="initial"
+      animate="animate"
+      className="flex gap-1"
+    >
+      <motion.div variants={variants} className="h-12 w-2 bg-white" />
+      <motion.div variants={variants} className="h-12 w-2 bg-white" />
+      <motion.div variants={variants} className="h-12 w-2 bg-white" />
+      <motion.div variants={variants} className="h-12 w-2 bg-white" />
+      <motion.div variants={variants} className="h-12 w-2 bg-white" />
+    </motion.div>
+  );
+};
+
+const BarLoaderWrapper = ({ isLoading }) => {
+  if (!isLoading) return null;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-600 bg-opacity-50 z-50">
+      <div className="grid place-content-center bg-[#61a680] px-4 py-10 rounded-lg">
+        <BarLoader />
+      </div>
+    </div>
+  );
+};
+
+export default BarLoaderWrapper;
+
