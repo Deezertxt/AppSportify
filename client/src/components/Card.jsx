28b82d1de42f1fa7299b97b724077f347e81f76e
Shoutt4 <jhonvergara437@gmail.com>
2024-10-22 01:26:09 -0400

    cards

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -8,18 +8,31 @@ function concertir(cadena) {
 }
 
 function Card({ title, author, description, coverUrl, onClick }) {
+    const handleListenClick = (event) => {
+        event.stopPropagation(); // Evita que el clic en el botón propague el evento
+        onClick(); // Llama a la función onClick que se pasa como prop
+    };
+
     return (
-        <div 
-            className="p-4 md:w-1/3" 
-            onClick={onClick}
-            style={{ cursor: "pointer" }}
-        >
-            <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:shadow-lg">
-                <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={coverUrl} alt="audiobook cover" />
-                <div className="p-6">
-                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3">{concertir(title)}</h1>
-                    <h2 className="tracking-widest text-s title-font font-medium text-gray-400 mb-1">{author}</h2>
-                    <p className="leading-relaxed mb-3">{description}</p>
+        <div className="card-container p-4 md:w-1/3">
+            <div className="card h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:shadow-lg">
+                <img 
+                    className="card-image lg:h-48 md:h-36 w-full object-cover" 
+                    src={coverUrl} 
+                    alt="audiobook cover" 
+                />
+                <div className="card-content p-6">
+                    <h1 className="card-title title-font text-lg font-medium text-gray-900 mb-3">{concertir(title)}</h1>
+                    <h2 className="card-author tracking-widest text-s title-font font-medium text-gray-400 mb-1">{author}</h2>
+                    <p className="card-description leading-relaxed mb-3">{description}</p>
+                    <div className="card-footer flex justify-end mt-4">
+                        <button 
+                            className="card-button bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600 transition duration-300" // Color verde medio
+                            onClick={handleListenClick} // Asocia el clic al botón
+                        >
+                            Escuchar Ahora
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -27,5 +40,3 @@ function Card({ title, author, description, coverUrl, onClick }) {
 }
 
 export default Card;
-
-
