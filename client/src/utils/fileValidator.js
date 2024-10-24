f33df77d965f278945a5918e039b0a83b2e99260
zebal <zeballosperedodylanamin@gmail.com>
2024-10-23 20:10:08 -0400

    fix:bug pdf

--- client/src/utils/fileValidator.js
+++ client/src/utils/fileValidator.js
@@ -1,37 +1,18 @@
-export const isPDF = (file) => {
-    if (!file || !file.name) {
-        return false;
-    }
-    const fileExtension = file.name.split('.').pop().toLowerCase();
-    return fileExtension === 'pdf';
-};
+// Validación del documento PDF
+ export const isValidPdf = async (file) => {
+    const maxSizeInMB = 50; // Tamaño máximo en MB
+    const allowedTypes = ["application/pdf"]; // Tipos de archivo permitidos
 
-export const hasTextInPDF = async (file) => {
-    if (!isPDF(file)) {
-        return false;
+    // Validar tipo de archivo
+    if (!allowedTypes.includes(file.type)) {
+        return { isValid: false, error: "Solo se permiten archivos PDF." };
     }
 
-    try {
-        const fileArrayBuffer = await file.arrayBuffer();
-
-        const pdf = await pdfjsLib.getDocument({ data: fileArrayBuffer }).promise;
-
-        let hasText = false;
-
-        const numPagesToCheck = Math.min(pdf.numPages, 5);
-        for (let i = 1; i <= numPagesToCheck; i++) {
-            const page = await pdf.getPage(i);
-            const textContent = await page.getTextContent();
-
-            if (textContent.items.length > 0) {
-                hasText = true;
-                break;
-            }
-        }
-
-        return hasText;
-    } catch (error) {
-        console.error('Error reading PDF:', error);
-        return false;
+    // Validar tamaño
+    const maxSizeInBytes = maxSizeInMB * 1024 * 1024; // Convertir MB a Bytes
+    if (file.size > maxSizeInBytes) {
+        return { isValid: false, error: `El tamaño del archivo no debe exceder ${maxSizeInMB} MB.` };
     }
+
+    return { isValid: true };
 };
