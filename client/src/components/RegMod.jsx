da0046205bdb59f920d062d007206ef2dc2d58ab
Mau <ortegacamachomauricio@gmail.com>
2024-10-26 13:42:32 -0400

    boton para probar modal

---
+++ client/src/components/RegMod.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+function RegMod({ isOpen, closeModal }) {
+    return (
+        <div>
+            <div>
+                <div>
+                    <label>
+                        
+                    </label>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default RegMod;
