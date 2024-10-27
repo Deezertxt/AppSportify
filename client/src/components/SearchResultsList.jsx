63f5fdbc3f0a8ccff3dd0448753ae8a8d3fb3290
DIAZ <owenstokd@gmail.com>
2024-10-27 01:39:41 -0400

    feat: resultados de busqueda como componente (work in progress)

--- client/src/components/SearchResultsList.jsx
+++ client/src/components/SearchResultsList.jsx
@@ -1,33 +1,15 @@
 import React from "react";
+import { SearchResult } from "./SearchResult";
 
-export const SearchResultsList = () => {
+export const SearchResultsList = ({results}) => {
     return (
         <>
             <div className="results-list w-full bg-gray flex flex-col shadow-none rounded-lg mt-4 max-h-[300px] overflow-y-scroll">
-                <div>a</div>
-                <div>b</div>
-                <div>c</div>
-                <div>aaaaaaaaaaa</div>
-                <div>a</div>
-                <div>b</div>
-                <div>c</div>
-                <div>aaaaaaaaaaa</div>
-                <div>a</div>
-                <div>b</div>
-                <div>c</div>
-                <div>aaaaaaaaaaa</div>
-                <div>a</div>
-                <div>b</div>
-                <div>c</div>
-                <div>aaaaaaaaaaa</div>
-                <div>a</div>
-                <div>b</div>
-                <div>c</div>
-                <div>aaaaaaaaaaa</div>
-                <div>a</div>
-                <div>b</div>
-                <div>c</div>
-                <div>aaaaaaaaaaa</div>
+              {
+                results.map((result, id) => {
+                    return <SearchResult result={result} key={id} />
+                })
+              }
             </div>
         </>
     );
