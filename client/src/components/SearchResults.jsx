8a0d7a50dbb29502252bd5ca44fb1b2a1a9c0660
DIAZ <owenstokd@gmail.com>
2024-10-31 14:32:07 -0400

    feat: refactorizaciondecomponentes

--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Card from "../components/Card";
-
-import { SearchOptions } from "./SearchOptions";
 import { useLocation, useNavigate } from "react-router-dom";
 
 //resultados de busqueda al darle a la lupa
 export const SearchResults = () => {
     const [audiobooks, setAudiobooks] = useState([]);
     const location = useLocation();
-    const entrada = location.state?.input || "";
+    const entrada = location.state?.input || ""; //recibo la entrada para filtrar y mapear coincidencias al dar 'Enter' o lupa
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -20,18 +18,7 @@ export const SearchResults = () => {
                     fetch("http://localhost:3000/api/audiobook/get/")
                         .then((response) => response.json())
                         .then((json) => {
-                            console.log(
-                                "----------------------------------------------"
-                            );
-                            //console.log(typeof json);
-                            console.log("AAAAAAAAAAAA");
-                            console.log("si audiobooks esta vacio:" + json);
-                            console.log(entrada.toLowerCase());
-                            console.log("ENCONTRADOS:    " + json);
                             setAudiobooks(json);
-                            console.log(
-                                "------------------------------------------"
-                            );
                         });
                 } catch (error) {
                     console.error("Error al obtener audiolibros:", error);
@@ -41,11 +28,6 @@ export const SearchResults = () => {
                     fetch("http://localhost:3000/api/audiobook/get/")
                         .then((response) => response.json())
                         .then((json) => {
-                            console.log(
-                                "----------------------------------------------"
-                            );
-                            //console.log(typeof json);
-                            console.log(json);
                             const results = json.filter((audiobook) => {
                                 return (
                                     entrada &&
@@ -56,12 +38,7 @@ export const SearchResults = () => {
                                         .includes(entrada.toLowerCase())
                                 );
                             });
-                            console.log(entrada.toLowerCase());
-                            console.log("ENCONTRADOS:    " + results);
                             setAudiobooks(results);
-                            console.log(
-                                "------------------------------------------"
-                            );
                         });
                 } catch (error) {
                     console.error("Error al obtener audiolibros:", error);
@@ -77,10 +54,6 @@ export const SearchResults = () => {
     };
     return (
         <div>
-            <div className="px-20">
-                {/* <div className=''>Filtrar por: </div> */}
-                <SearchOptions />
-            </div>
             <div className="max-w-5xl mx-auto mt-8">
                 {Array.isArray(audiobooks) && audiobooks.length > 0 ? (
                     <div className="flex flex-wrap -m-4">
