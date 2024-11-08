2b8dc6d9f6eed03addd5b1ccce754b4bb90ee783
DIAZ <owenstokd@gmail.com>
2024-11-08 00:18:43 -0400

    fix: alineacion botones y buscador

--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from "react";
 import Card from "../components/Card";
 import { useLocation, useNavigate } from "react-router-dom";
 import SearchBar from "../components/SearchBar";
-import BarLoaderWrapper from './BarLoader';
+import BarLoaderWrapper from "./BarLoader";
 const SearchResults = () => {
     const [audiobooks, setAudiobooks] = useState([]);
     const [filter, setFilter] = useState("Todo"); // Estado para controlar el filtro seleccionado
     const location = useLocation();
-    const entrada = location.state?.input || ""; 
+    const entrada = location.state?.input || "";
     const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(false);
 
@@ -15,18 +15,28 @@ const SearchResults = () => {
         const fetchAudiobooks = async () => {
             setIsLoading(true);
             try {
-                const response = await fetch("http://localhost:3000/api/audiobook/get/");
+                const response = await fetch(
+                    "http://localhost:3000/api/audiobook/get/"
+                );
                 const json = await response.json();
                 const filteredResults = json.filter((audiobook) => {
                     if (filter === "Título") {
-                        return audiobook.title?.toLowerCase().includes(entrada.toLowerCase());
+                        return audiobook.title
+                            ?.toLowerCase()
+                            .includes(entrada.toLowerCase());
                     } else if (filter === "Autor") {
-                        return audiobook.author?.toLowerCase().includes(entrada.toLowerCase());
+                        return audiobook.author
+                            ?.toLowerCase()
+                            .includes(entrada.toLowerCase());
                     } else {
                         // Filtro "Todo" muestra coincidencias en título o autor
                         return (
-                            (audiobook.title?.toLowerCase().includes(entrada.toLowerCase())) ||
-                            (audiobook.author?.toLowerCase().includes(entrada.toLowerCase()))
+                            audiobook.title
+                                ?.toLowerCase()
+                                .includes(entrada.toLowerCase()) ||
+                            audiobook.author
+                                ?.toLowerCase()
+                                .includes(entrada.toLowerCase())
                         );
                     }
                 });
@@ -46,34 +56,46 @@ const SearchResults = () => {
 
     return (
         <div>
-            <BarLoaderWrapper isLoading={isLoading}/>
+            <BarLoaderWrapper isLoading={isLoading} />
             <div className="px-20">
                 <SearchBar />
             </div>
+            <div className="flex items-center pl-24">Filtrar por:</div>
+            <div className="flex items-center mb-4 pl-24">
+                
+                <button
+                    onClick={() => setFilter("Todo")}
+                    className={`px-4 py-2 mr-2 ${
+                        filter === "Todo"
+                            ? "bg-blue-500 text-white"
+                            : "bg-gray-200"
+                    }`}
+                >
+                    Todos
+                </button>
+                <button
+                    onClick={() => setFilter("Título")}
+                    className={`px-4 py-2 mr-2 ${
+                        filter === "Título"
+                            ? "bg-blue-500 text-white"
+                            : "bg-gray-200"
+                    }`}
+                >
+                    Título
+                </button>
+                <button
+                    onClick={() => setFilter("Autor")}
+                    className={`px-4 py-2 ${
+                        filter === "Autor"
+                            ? "bg-blue-500 text-white"
+                            : "bg-gray-200"
+                    }`}
+                >
+                    Autor
+                </button>
+            </div>
             <div className="max-w-5xl mx-auto mt-8">
                 {/* Botones de filtro */}
-                Filtrar por:
-                <div className="flex justify-start mb-4">
-                    <button
-                        onClick={() => setFilter("Todo")}
-                        className={`px-4 py-2 mr-2 ${filter === "Todo" ? "bg-blue-500 text-white" : "bg-gray-200"}`}
-                    >
-                        Todos
-                    </button>
-                    <button
-                        onClick={() => setFilter("Título")}
-                        className={`px-4 py-2 mr-2 ${filter === "Título" ? "bg-blue-500 text-white" : "bg-gray-200"}`}
-                    >
-                        Título
-                    </button>
-                    <button
-                        onClick={() => setFilter("Autor")}
-                        className={`px-4 py-2 ${filter === "Autor" ? "bg-blue-500 text-white" : "bg-gray-200"}`}
-                    >
-                        Autor
-                    </button>
-                </div>
-                
                 {Array.isArray(audiobooks) && audiobooks.length > 0 ? (
                     <div className="flex flex-wrap -m-4">
                         {audiobooks.map((audiobook) => (
