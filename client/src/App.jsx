da74df7ac2565a88b35c798a5b6b701ca2c27098
zebal <zeballosperedodylanamin@gmail.com>
2024-11-17 16:25:45 -0400

    feat: rutas actualizadas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,50 @@
 import { Route, Routes } from 'react-router-dom';
 
 import MainLayout from './pages/MainLayout';
-import Biblioteca from './pages/Biblioteca';
+import Inicio from './pages/Inicio';
 import AudioLibroReproductor from './pages/AudiolibroRepro';
 import Publicar from './pages/Publicar';
 import PanelAdmin from "./pages/PanelAdmin";
+import Biblioteca from "./pages/Biblioteca";
 import Actualizar from "./pages/Actualizar";
-import InicioSesion from './pages/InicioSesion';
-import Registro from './components/ModalRegistro';
 import TasksPage from './pages/Taskpage';
 import HeroSection from './pages/HeroSection';
 import Preview from './pages/Preview';
 import Reproductor from './pages/Reproductor';
-import SearchResults from './components/SearchResults';
+import SearchResults from './components/search/SearchResults';
+import Explorar from './pages/Explorar';
+import Categorias from './pages/Categorias';
+import Reseña from './pages/Reseña';
+
 import { AuthProvider } from './context/authContext';
 import { ProtectedRoutes } from './context/ProtectedRoutes';
+import { LibraryProvider } from './context/libraryContext';
 
 function App() {
     return (
         <AuthProvider>
-            <Routes>
-                <Route path="/" element={<HeroSection />} />
-                <Route path="/login" element={<InicioSesion />} />
-                <Route path="/registro" element={<Registro />} />
-                
-                {/* MainLayout envuelve las rutas protegidas */}
-                <Route element={<MainLayout />}>
-                    <Route path="/libros" element={<ProtectedRoutes><Biblioteca /></ProtectedRoutes>} />
-                    <Route path="/reproductor/:id" element={<AudioLibroReproductor />} />
-                    <Route path="/escuchar/:id" element={<Reproductor />} />
-                    <Route path="/panelAdmin" element={<PanelAdmin />} />
-                    <Route path="/publicar" element={<Publicar />} />
-                    <Route path="/actualizar/:id" element={<Actualizar />} />
-                    <Route path="/taskpage" element={<TasksPage />} />
-                    <Route path="/preview/:id" element={<Preview />} />
-                    <Route path="/buscar" element={<SearchResults />} />
-                </Route>
-            </Routes>
+            <LibraryProvider>
+                <Routes>
+                    <Route path="/" element={<HeroSection />} />
+                    {/* MainLayout envuelve las rutas protegidas */}
+                    <Route element={<MainLayout />}>
+
+                        <Route path="/libros" element={<ProtectedRoutes><Inicio /></ProtectedRoutes>} />
+                        <Route path="/biblioteca" element={<ProtectedRoutes><Biblioteca /></ProtectedRoutes>} />
+                        <Route path="/reproductor/:id" element={<ProtectedRoutes><AudioLibroReproductor /></ProtectedRoutes>} />
+                        <Route path="/escuchar/:id" element={<ProtectedRoutes><Reproductor /></ProtectedRoutes>} />
+                        <Route path="/panelAdmin" element={<ProtectedRoutes><PanelAdmin /></ProtectedRoutes>} />
+                        <Route path="/publicar" element={<ProtectedRoutes><Publicar /></ProtectedRoutes>} />
+                        <Route path="/actualizar/:id" element={<ProtectedRoutes><Actualizar /></ProtectedRoutes>} />
+                        <Route path="/taskpage" element={<ProtectedRoutes><TasksPage /></ProtectedRoutes>} />
+                        <Route path="/preview/:id" element={<ProtectedRoutes><Preview /></ProtectedRoutes>} />
+                        <Route path="/buscar" element={<ProtectedRoutes><SearchResults /></ProtectedRoutes>} />
+                        <Route path="/explorar" element={<ProtectedRoutes><Explorar /></ProtectedRoutes>} />
+                        <Route path="/categorias/:id" element={<ProtectedRoutes><Categorias /></ProtectedRoutes>} />
+                        <Route path="/resenia/:id" element={<ProtectedRoutes><Reseña /></ProtectedRoutes>} />
+                    </Route>
+                </Routes>
+            </LibraryProvider>
         </AuthProvider>
     );
 }
