3fb4dbf3ee3881944c32526f346a781a640e3fd6
Shoutt4 <jhonvergara437@gmail.com>
2024-11-20 23:02:29 -0400

    perfil Terminado

--- client/src/pages/PerfilUser.jsx
+++ client/src/pages/PerfilUser.jsx
@@ -1,38 +1,58 @@
 import React, { useState } from "react";
-import Slider from "react-slick"; // Importa el Slider de react-slick
+import Slider from "react-slick"; 
 import Card from "./Card";
+import { BsBookmarkCheckFill  ,BsCheckSquareFill ,BsFillPatchCheckFill  } from "react-icons/bs";
 
+const guardados = [
+  
+ 
+ 
+ 
+ 
+   {
+    imageUrl:
+      "https://storage.googleapis.com/sportify-2/uploads/covers/WhatsApp Image 2024-11-01 at 21.41.20.jpeg",
+    title: "Voleibol",
+    author: "De altura",
+  },
+ 
+  
+  
+ 
+ 
+];
 
 const PerfilUser = () => {
   const [activeTab, setActiveTab] = useState("recomendado");
 
   // Configuración del carrusel
   const settings = {
-    dots: true, // Muestra los puntos de navegación
-    infinite: true, // Carrusel infinito
-    speed: 500, // Velocidad de transición
-    slidesToShow: 5, // Muestra 5 tarjetas a la vez
-    slidesToScroll: 1, // Desplaza una tarjeta a la vez
+  dots: true, // Muestra los puntos de navegación
+  infinite: false, // Desactiva el desplazamiento infinito
+  speed: 500, // Velocidad de transición
+  slidesToShow: Math.min(5, guardados.length), // Muestra el menor valor entre 5 y el número de tarjetas
+  slidesToScroll: 1, 
     responsive: [
       {
         breakpoint: 1024,
-        settings: {
-          slidesToShow: 3, 
-          slidesToScroll: 1,
+      settings: {
+        slidesToShow: Math.min(3, guardados.length), 
+        slidesToScroll: 1,
         },
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2, 
+          slidesToShow: Math.min(2, guardados.length),
           slidesToScroll: 1,
         },
       },
     ],
   };
-
+  
   return (
     <div className="max-w-xl mx-auto bg-white shadow-md rounded-lg overflow-hidden">
+
       <div className="flex flex-col items-center p-4">
         <img
           className="w-20 h-20 rounded-full shadow-md sm:w-24 sm:h-24"
@@ -55,38 +75,49 @@ const PerfilUser = () => {
         </p>
       </div>
 
+      {/* Tabs */}
       <div className="border-t border-gray-200">
         <div className="flex justify-center bg-white text-gray-800">
-          <button
-            className={`flex-1 py-2 text-center border-b-2 ${
-              activeTab === "recomendado"
-                ? "border-blue-500 font-bold text-blue-500"
-                : "border-transparent hover:text-blue-500 hover:border-blue-500"
-            }`}
-            onClick={() => setActiveTab("recomendado")}
-          >
-            Recomendado
-          </button>
-          <button
-            className={`flex-1 py-2 text-center border-b-2 ${
-              activeTab === "guardado"
-                ? "border-blue-500 font-bold text-blue-500"
-                : "border-transparent hover:text-blue-500 hover:border-blue-500"
-            }`}
-            onClick={() => setActiveTab("guardado")}
-          >
-            Guardado
-          </button>
-          <button
-            className={`flex-1 py-2 text-center border-b-2 ${
-              activeTab === "terminados"
-                ? "border-blue-500 font-bold text-blue-500"
-                : "border-transparent hover:text-blue-500 hover:border-blue-500"
-            }`}
-            onClick={() => setActiveTab("terminados")}
-          >
-            Terminados
-          </button>
+        <button
+  className={`flex-1 py-2 text-center border-b-2 ${
+    activeTab === "recomendado"
+      ? "border-blue-500 font-bold text-blue-500"
+      : "border-transparent hover:text-blue-500 hover:border-blue-500"
+  } inline-flex items-center justify-center space-x-2 text-lg`}
+  onClick={() => setActiveTab("recomendado")}
+>
+  <BsFillPatchCheckFill className="text-lg" size={24} />
+  <span>Recomendado</span>
+</button>
+
+<button
+  className={`flex-1 py-2 text-center border-b-2 ${
+    activeTab === "guardado"
+      ? "border-blue-500 font-bold text-blue-500"
+      : "border-transparent hover:text-blue-500 hover:border-blue-500"
+  } inline-flex items-center justify-center space-x-2 text-lg`}
+  onClick={() => setActiveTab("guardado")}
+>
+  <span className="flex items-center space-x-2">
+    <BsBookmarkCheckFill className="text-lg" size={24} />
+    <span>Guardado</span>
+  </span>
+</button>
+
+<button
+  className={`flex-1 py-2 text-center border-b-2 ${
+    activeTab === "terminados"
+      ? "border-blue-500 font-bold text-blue-500"
+      : "border-transparent hover:text-blue-500 hover:border-blue-500"
+  } inline-flex items-center justify-center space-x-2 text-lg`}
+  onClick={() => setActiveTab("terminados")}
+>
+  <span className="flex items-center space-x-2">
+    <BsCheckSquareFill className="text-lg" size={24} />
+    <span>Terminados</span>
+  </span>
+</button>
+
         </div>
 
         <div className="p-3">
