c6f95349f63551ab7a747bb38c02a7fcf81adf4c
zebal <zeballosperedodylanamin@gmail.com>
2024-11-24 13:36:35 -0400

    feat:rutas nuevas

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { registerOrLoginWithGoogle,createUser, deleteUser, getUserById, updateUser } = require("../controller/userController");
+const { registerOrLoginWithGoogle,createUser, deleteUser, getUserById, getUserByEmail, updateUser, toggleProfilePrivacy } = require("../controller/userController");
 
 router.post('/registergoogle', registerOrLoginWithGoogle);
 router.post('/register', createUser);
 router.delete('/delete/:id', deleteUser);
 router.get('/get/:id',getUserById);
+router.get('/getemail/:email',getUserByEmail);
 router.put('/put/:id',updateUser);
+router.patch('/privacy/:id', toggleProfilePrivacy);
 
 module.exports = router;
