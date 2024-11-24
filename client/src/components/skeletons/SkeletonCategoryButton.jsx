14c1655fafaadaa4d0ca53d0e7bdbda5e691ca7d
zebal <zeballosperedodylanamin@gmail.com>
2024-11-24 13:15:50 -0400

    feat:NewSkeletonButton

--- client/src/components/skeletons/SkeletonCategoryButton.jsx
+++ client/src/components/skeletons/SkeletonCategoryButton.jsx
@@ -2,8 +2,7 @@ import React from "react";
 
 const SkeletonCategoryButton = () => {
     return (
-        <div className="w-[160px] h-[40px] bg-gray-300 rounded-lg animate-pulse"></div>
-        
+        <div className="w-full h-[50px] sm:h-[60px] bg-gray-300 dark:bg-gray-700 rounded-lg animate-pulse"></div>
     );
 };
 
