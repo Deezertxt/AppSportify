998f6a3ca9b74f51d5302eb43b3dc4b166f762aa
zebal <zeballosperedodylanamin@gmail.com>
2024-11-24 13:29:32 -0400

    feat:ModoOscuro actualizado pages

--- client/src/pages/MainLayout.jsx
+++ client/src/pages/MainLayout.jsx
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
-import DropdownMenu from "../components/DropdownMenu";
+import Sidebar from "../components/DropdownMenu";
 import Footer from "../components/Footer";
 
 const MainLayout = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(true); // Estado para controlar si el sidebar está abierto o no
+  const [darkMode, setDarkMode] = useState(false); // Estado para controlar el modo oscuro
+
+  // Cambiar el tema según el estado de darkMode
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
+
+  // Función para alternar el modo oscuro
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
     <div className="flex min-h-screen">
       {/* Sidebar */}
-      <DropdownMenu />
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
       {/* Page Content */}
-      <div className="flex-grow flex flex-col min-h-screen">
-        {/* Main content */}
-        <main className="flex-grow p-7 bg-second">
+      <div
+        className={`flex-grow flex flex-col min-h-screen transition-all duration-300 ease-in-out ${
+          darkMode ? "bg-gradient-to-r from-[#023047] to-[#082938] text-white" : "bg-white text-gray-800"
+        }`}
+      >
+        {/* Toggle dark mode button */}
+        <button
+          onClick={toggleDarkMode}
+          className="p-2 fixed top-5 right-5 border-indigo-900 text-white rounded-full transition duration-200 hover:bg-indigo-600"
+        >
+          {darkMode ? "🌙" : "🌞"}
+        </button>
+
+        {/* Main Content */}
+        <main className="flex-grow p-7">
           <Outlet /> {/* Renderiza las rutas hijas aquí */}
         </main>
 
